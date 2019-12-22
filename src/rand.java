b5219b3ab8459619099c96dc6521fd3e7a497f41
PtitLu62 <lucasdelestreepro@gmail.com>
2019-12-23 00:51:56 +0100

    ajout de nouvelles fonctionnalités

--- src/rand.java
+++
@@ -1,9 +0,0 @@
-import extensions.CSVFile;
-class rand extends Program{
-
-    void algorithm(){
-        CSVFile fichiercsv = loadCSV("questions_reponses_stock.csv");
-	int q1=0;
-        printf("\x1b[${bg};2;${red};${green};${blue}m\n")
-    }
-}
